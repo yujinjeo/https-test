20c8dba48b271e43cae57df9dac5e6392ffbd502
sophie_lavender <wjddbwls@ewhain.net>
2024-05-22 18:33:31 +0900

    init : create app

---
+++ src/main/java/com/test/httpsdeploy/HttpsdeployApplication.java
@@ -0,0 +1,13 @@
+package com.test.httpsdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpsdeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpsdeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/httpsdeploy/HttpsdeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.httpsdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpsdeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
